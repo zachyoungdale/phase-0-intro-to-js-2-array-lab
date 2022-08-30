160fd62afd3001d1b85bebb1dd16bbde07cd95d2
Zachary Youngdale <zyoungdale@gmail.com>
2022-08-30 16:09:45 -0400

    commit tree 39497fd999b10e5f8a8c3a34cbf7e0fc4fc5a08f

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+destructivelyAppendCat("Ralph");
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+destructivelyPrependCat();
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+destructivelyRemoveLastCat();
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+destructivelyRemoveFirstCat();
+
+function appendCat(name) {
+    const newCats = [...cats, name];
+    return newCats;
+}
+appendCat();
+
+function prependCat(name) {
+    const newCats = [name, ...cats];
+    return newCats;
+}
+prependCat();
+
+function removeLastCat() {
+    const newCats = cats.slice(0, -1);
+    return newCats;
+}
+removeLastCat();
+
+function removeFirstCat() {
+    const newCats = cats.slice(1);
+    return newCats;
+} removeFirstCat();
